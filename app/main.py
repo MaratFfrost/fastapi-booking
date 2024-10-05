61ac0bbeb107f75d3f3fa6686c93a26d4699106b
MarayFfrost <morozovmarat4@gmail.com>
2024-10-05 04:04:15 +0300

    Первая версия админки

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from sqladmin import Admin, ModelView
+
 
 from collections.abc import AsyncIterator
 from contextlib import asynccontextmanager
@@ -10,8 +12,10 @@ from fastapi_cache.decorator import cache
 from redis import asyncio as aioredis
 
 from app.bookings.router import router as router_booking
+from app.users.models import Users
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
+from app.database import engine
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
@@ -28,3 +32,16 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(router_users)
 app.include_router(router_booking)
 app.include_router(router_hotels)
+
+
+admin = Admin(app, engine)
+
+
+class UserAdmin(ModelView, model=Users):
+    column_list = [Users.id, Users.email]
+    can_delete = False
+    can_edit = False
+
+
+
+admin.add_view(UserAdmin)
